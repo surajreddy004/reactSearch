42010bfb1a5f695fb971a574fd921ca4f647c714
surajreddy004 <65901686+surajreddy004@users.noreply.github.com>
2021-02-19 16:14:06 +0100

    list

---
+++ src/Component/Listing/ListingApi.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListingApi extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>ListinApi</h2>
+            </div>
+        )
+    }
+}
+
+export default ListingApi;
--- src/Component/Routing.js
+++ src/Component/Routing.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import {BrowserRouter} from 'react-router-dom';
 import Home from './Home/Main'
+import ListingApi from './Listing/ListingApi';
 
 const Routing=()=>{
 return(
     <BrowserRouter>
     <div>
         <Header/>
-        <Home/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/list/:id" component={ListingApi}/>
+
         <Footer/>
     </div>
     </BrowserRouter>
