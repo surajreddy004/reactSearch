288871f6e4b92c8fd2f3c0031d92ba4d0e6db7af
surajreddy004 <65901686+surajreddy004@users.noreply.github.com>
2021-02-19 03:18:12 +0100

    Quicksearch

--- src/Component/Home/QuickSearch.js
+++ src/Component/Home/QuickSearch.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import QuickDisplay from './QuickDisplay';
+
+const Quickurl="https://developerfunnel.herokuapp.com/booking"
 
 class QuickSearch extends React.Component{
     constructor(props){
         super(props)
+
+        this.state={
+            tripType:''
+        }
     }
 
 
 render(){
 return(
     
-        <h1>QuickSearch</h1>
+        <div>
+            <QuickDisplay tripData={this.state.tripType}/>
+        </div>
     
 )
+}
+
+componentDidMount(){
+    fetch(Quickurl)
+    .then((res)=> res.json())
+    .then((data)=>{this.setState({tripType:data})})
 }
 }
 
