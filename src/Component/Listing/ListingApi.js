141f57b66c3eabbfd0860be458cfd711bd4a89f8
surajreddy004 <65901686+surajreddy004@users.noreply.github.com>
2021-02-21 15:47:48 +0100

    listing according to quicksearch

--- src/Component/Listing/ListingApi.js
+++ src/Component/Listing/ListingApi.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React,{Component} from 'react';
+import axios from 'axios';
+import ListingDisplay from './ListingDisplay';
 
-class ListingApi extends React.Component{
+const url = "https://developerfunnel.herokuapp.com/hotellist"
+
+class ListingApi extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            hotellist:''
+        }
+    }
+
+    setDataPerFilter=(soretedData)=>{
+        this.setState({hotellist:soretedData})
+    }
+
     render(){
         return(
             <div>
-                <h2>ListinApi</h2>
+                <div className="row">
+                    <div className="col-md-10">
+                        <ListingDisplay listdata={this.state.hotellist}/>
+                    </div>
+                </div>
+                
             </div>
         )
+    }
+
+    componentDidMount(){
+        var tripid = this.props.match.params.id;
+        sessionStorage.setItem('tripid',tripid)
+        axios.get(`${url}/${tripid}`)
+        .then((response) => {this.setState({hotellist:response.data})})
     }
 }
 
